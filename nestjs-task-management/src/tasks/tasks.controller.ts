0b9d27f5f33d7aba47c97ad513f1cd7c4de525c2
Abhijeet Sakhalkar <abhijeet.sakhalkar@wipro.com>
2022-08-02 01:46:26 +0530

    tasks - create, get all, get by id and delete

--- nestjs-task-management/src/tasks/tasks.controller.ts
+++ nestjs-task-management/src/tasks/tasks.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) { }
 
     @Get()
-    getAllTasks() {
+    getAllTasks(): Task[] {
         return this.tasksService.getAllTasks();
+    }
+
+    @Get("/:id")
+    getTaskById(@Param("id") id: string): Task {
+        return this.tasksService.getTaskById(id);
+    }
+
+    @Delete("/:id")
+    deleteTaskById(@Param("id") id: string): void {
+        this.tasksService.deleteTaskById(id);
+    }
+
+    @Post()
+    createTask(
+        @Body() createTaskDto: CreateTaskDto
+    ): Task {
+        return this.tasksService.createTask(createTaskDto);
     }
 }
--- nestjs-task-management/src/tasks/tasks.service.ts
+++ nestjs-task-management/src/tasks/tasks.service.ts
@@ -1,11 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from "uuid";
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
-    private tasks = [];
+    private tasks: Task[] = [];
 
     // By default TS makes method public
-    getAllTasks() {
+    getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    getTaskById(id: string): Task {
+        return this.tasks.find((task) => task.id === id);
+    }
+
+    deleteTaskById(id: string): void {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+    }
+
+    createTask(createTaskDto: CreateTaskDto): Task {
+
+        const { title, description } = createTaskDto;
+        const task: Task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN
+        }
+        this.tasks.push(task);
+        return task;
     }
 }
