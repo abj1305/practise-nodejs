4faab1929c5dc43c464cdc6b4c1d346a2217e86b
Abhijeet Sakhalkar <abhijeet.sakhalkar@wipro.com>
2022-09-20 21:49:08 +0530

    CRUD ops on DB

--- nestjs-task-management/src/tasks/tasks.service.ts
+++ nestjs-task-management/src/tasks/tasks.service.ts
@@ -13,28 +13,10 @@ export class TasksService {
         private tasksRepository: TasksRepository,
     ) { }
 
-    // By default TS makes method public
-    // getAllTasks(): Task[] {
-    //     return this.tasks;
-    // }
 
-    // getTasksWithFilter(filterDto: GetTaskFilterDto): Task[] {
-    //     const { status, search } = filterDto;
-    //     let tasks = this.getAllTasks();
-    //     if (status) {
-    //         tasks = this.tasks.filter((task) => task.status === status);
-    //     }
-    //     if (search) {
-    //         tasks = this.tasks.filter((task) => {
-    //             if (task.title.includes(search) || task.description.includes(search)) {
-    //                 return true;
-    //             }
-    //             return false;
-    //         })
-    //     }
-
-    //     return tasks;
-    // }
+    getTasks(filterDto: GetTaskFilterDto): Promise<Task[]> {
+        return this.tasksRepository.getTasks(filterDto);
+    }
 
 
     async getTaskById(id: string): Promise<Task> {
@@ -45,35 +27,21 @@ export class TasksService {
         return found;
     }
 
-    // getTaskById(id: string): Task {
-    //     const found = this.tasks.find((task) => task.id === id);
-    //     if (!found) {
-    //         throw new NotFoundException(`Task with ID: "${id}" not found`);
-    //     }
-    //     return found;
-    // }
+    async deleteTaskById(id: string): Promise<void> {
+        const result = await this.tasksRepository.delete(id);
+        if (result.affected === 0) {
+            throw new NotFoundException(`Task with ID: "${id}" not found`);
+        }
+    }
 
-    // deleteTaskById(id: string): void {
-    //     const found = this.getTaskById(id);
-    //     this.tasks = this.tasks.filter((task) => task.id !== id);
-    // }
+    createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+        return this.tasksRepository.createTask(createTaskDto);
+    }
 
-    // createTask(createTaskDto: CreateTaskDto): Task {
-
-    //     const { title, description } = createTaskDto;
-    //     const task: Task = {
-    //         id: uuid(),
-    //         title,
-    //         description,
-    //         status: TaskStatus.OPEN
-    //     }
-    //     this.tasks.push(task);
-    //     return task;
-    // }
-
-    // updateTaskStatus(id: string, status: TaskStatus): Task {
-    //     const task = this.getTaskById(id);
-    //     task.status = status;
-    //     return task;
-    // }
+    async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
+        const task = await this.getTaskById(id);
+        task.status = status;
+        await this.tasksRepository.save(task);
+        return task;
+    }
 }
